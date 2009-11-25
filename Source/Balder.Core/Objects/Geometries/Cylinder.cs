c0614647660040d5ad4fabaa07410b9d43e4ad39
Einar Ingebrigtsen <einar@dolittle.com>
2009-11-25 17:58:11 +0100

    Added License to all source files

--- Source/Balder.Core/Objects/Geometries/Cylinder.cs
+++ Source/Balder.Core/Objects/Geometries/Cylinder.cs
@@ -1,27 +1,27 @@
-﻿namespace Balder.Core.Objects.Geometries
+﻿#region License
+//
+// Author: Einar Ingebrigtsen <einar@dolittle.com>
+// Copyright (c) 2007-2009, DoLittle Studios
+//
+// Licensed under the Microsoft Permissive License (Ms-PL), Version 1.1 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the license at 
+//
+//   http://balder.codeplex.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+namespace Balder.Core.Objects.Geometries
 {
 	public class Cylinder : Geometry
 	{
 		public Cylinder(float radius, float height, int segments, int heightSegments)
 		{
-			/*
-                 Ok. Da er det lagd..      En liten forklaring: Cylinder er nå en klasse som ligger i Geometries - den arver fra Geometry. 
-             * I Geometry er det en injected property som heter GeometryContext - den bruker du for å lage vertices og faces
-             * (husk å bruke allocate... metodene først).  I interface som heter IContentManager har jeg nå lagt til en metode som
-             * heter CreateCylinder() - denne er også da implementert i implementasjonen og bruker NInject sin kernel for å lage Cylinder
-             * - grunnen til det er at GeometryContext er en injected property.
-                 Hvis du vil endre på argumentene som går inn på constructoren, så må du være nøye med navgivningen og gjøre 
-             * samme endringen i ContentManager sin CreateCylinder som setter opp argumentene som strings
-                 Jeg ser at jeg har lyst til å lage en Extension metode for å gjøre dette enklere - men det gjør jeg seinere.
-            */
-
-			//GeometryContext.AllocateVertices(10);
-			//for(int i = 0; i < 10; i++)
-			//{
-			//    GeometryContext.SetVertex(i,new Vertex(i,i,i));
-			//}
-
-			// + faces.. :)
 		}
 	}
 }
--- Source/Common/GlobalAssemblyInfo.cs
+++ Source/Common/GlobalAssemblyInfo.cs
@@ -1,3 +1,21 @@
+#region License
+//
+// Author: Einar Ingebrigtsen <einar@dolittle.com>
+// Copyright (c) 2007-2009, DoLittle Studios
+//
+// Licensed under the Microsoft Permissive License (Ms-PL), Version 1.1 (the "License")
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the license at 
+//
+//   http://balder.codeplex.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
 using System.Reflection;
 using System.Runtime.InteropServices;
 
@@ -6,7 +24,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Balder")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyCopyright("Copyright © 2007-2009, DoLittle Studios")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,5 +42,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.8.6.0")]
-[assembly: AssemblyFileVersion("0.8.6.0")]
+[assembly: AssemblyVersion("0.8.8.0")]
+[assembly: AssemblyFileVersion("0.8.8.0")]
